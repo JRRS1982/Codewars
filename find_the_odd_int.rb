c4a608d4fe4ba8efbc0be4196f1d25bf57e54190
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-05-02 22:54:53 +0100

    UPDATE: output update
    Just updating the styling as intend to do a few more katas (for proactice).

--- find_the_odd_int.rb
+++ find_the_odd_int.rb
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/find-the-odd-int/ruby
+# Ruby: 6kyu - Fundamentals
+# COMPLETE
+
 def find_it(seq)
   output = []
   counts = Hash.new(0)
@@ -13,3 +17,12 @@ def find_it(seq)
 end
 
 find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+p 5
+find_it([1,1,2,-2,5,2,4,4,-1,-2,5])
+p -1
+find_it([20,1,1,2,2,3,3,5,5,4,20,4,5])
+p 5
+find_it([10])
+p 10
+find_it([1,1,1,1,1,1,10,1,1,1,1])
+p 10
