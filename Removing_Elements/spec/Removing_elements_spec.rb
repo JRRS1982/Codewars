e40a1a8abb6096f00b51fb994c5a78087c2d5971
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-06-12 22:04:05 +0100

    change of method name

--- Removing_Elements/spec/Removing_elements_spec.rb
+++ Removing_Elements/spec/Removing_elements_spec.rb
@@ -3,6 +3,6 @@ require 'Removing_Elements'
 describe 'Removing_Elements' do
   it 'removes the second element from and array' do
     my_arr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep']
-    expect(remove_second(my_arr)).to eq(['Keep', 'Keep', 'Keep'])
+    expect(remove_every_other(my_arr)).to eq(['Keep', 'Keep', 'Keep'])
   end
 end
